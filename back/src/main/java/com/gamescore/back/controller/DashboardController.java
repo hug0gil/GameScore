1c892ef779b92c29f7641540c64edc0ccd61be40
kalandraka86 <hugogilb2005@gmail.com>
2025-11-19 13:50:12 +0100

    Cambio nombre review

--- back/src/main/java/com/gamescore/back/controller/DashboardController.java
+++ back/src/main/java/com/gamescore/back/controller/DashboardController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+
 @Controller
 @RequestMapping("/admin")
 @PreAuthorize("hasRole('ADMIN')")
