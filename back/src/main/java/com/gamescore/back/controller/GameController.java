3a0e606e0eaae541b47390ae9c3897fa9b07d56d
Orellana16 <daviorellana.o16@gmail.com>
2025-11-20 14:53:59 +0100

    Index y login mejorados

--- back/src/main/java/com/gamescore/back/controller/GameController.java
+++ back/src/main/java/com/gamescore/back/controller/GameController.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Random;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- back/src/main/java/com/gamescore/back/service/GameService.java
+++ back/src/main/java/com/gamescore/back/service/GameService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
